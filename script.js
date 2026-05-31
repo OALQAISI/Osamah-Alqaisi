1aa32fe5e12c2f6be981e9c1a6e2def8b93413eb
Osama Alqaisi <82491227+OALQAISI@users.noreply.github.com>
2026-05-31 20:47:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
+/*document.querySelectorAll('nav ul li a').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
+  });
+});
+*/
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href');
+
+    // only handle internal links like #hero
+    if (targetId.startsWith('#') && document.querySelector(targetId)) {
+      e.preventDefault();
+
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
   });
 });
